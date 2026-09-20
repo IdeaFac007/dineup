9073872449c72b438703315554cd4fa3aaee796d
IdeaFac007 <ideafac007@gmail.com>
2026-09-20 18:28:35 +0530

    perf: allow sitemap revalidation caching

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,8 +3,6 @@ import { createClient } from "../lib/supabase/server";
 
 export const revalidate = 3600;
 
-export const dynamic = "force-dynamic";
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const base = "https://dineupindia.com";
   const staticRoutes: MetadataRoute.Sitemap = [
